32d3abd6d4e3b7c64528ad1e9877b26608c83ca8
samihammoud <hammoud@chapman.edu>
2026-04-15 19:12:37 -0700

    Clerk login

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,33 @@
+import { SignedIn, SignedOut, SignIn } from "@clerk/clerk-react";
 import Header from "./components/Header";
 import Chat from "./components/Chat";
 import WorkspacePanel from "./components/WorkspacePanel";
 import { useAgent } from "./lib/useAgent";
 
-export default function App() {
-  const { messages, sendMessage, isConnected, openFiles, activeFile, setActiveFile, onLoadFile } =
-    useAgent();
+function AuthScreen() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#0a0a0f",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}>
+      <SignIn routing="hash" />
+    </div>
+  );
+}
+
+function MainApp() {
+  const {
+    messages,
+    sendMessage,
+    isConnected,
+    openFiles,
+    activeFile,
+    setActiveFile,
+    onLoadFile,
+  } = useAgent();
 
   return (
     <div className="app">
@@ -24,5 +46,18 @@ export default function App() {
         />
       </div>
     </div>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <SignedOut>
+        <AuthScreen />
+      </SignedOut>
+      <SignedIn>
+        <MainApp />
+      </SignedIn>
+    </>
   );
 }
